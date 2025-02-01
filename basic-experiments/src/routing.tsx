86be6b5fc067d2ab483e10224f176e3116791de4
Christopher Bartling <chris.bartling@gmail.com>
2025-01-31 23:07:29 -0600

    chore: reformatting

--- basic-experiments/src/routing.tsx
+++ basic-experiments/src/routing.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { WelcomePage } from './components/pages/WelcomePage';
-import {PricingCardsPage} from "./components/pages/PricingCardsPage";
+import { PricingCardsPage } from './components/pages/PricingCardsPage';
 
 export const Routing: FC = () => {
   return (
